de2d6324df07f323c32d09f0e0e7ba33b25eb4e9
Farid Ali <faridalicode@gmail.com>
2021-05-22 07:03:46 +0530

    added user controller functionality

--- controllers/articleController.js
+++ controllers/articleController.js
@@ -7,11 +7,13 @@ const { body, validationResult } = require("express-validator");
 exports.article_list = function (req, res, next) {
   Article.find({}, "title author")
     .populate("author")
-    .exec(function(err, all_articles) {
-      if (err) { return next(err); }
+    .exec(function (err, all_articles) {
+      if (err) {
+        return next(err);
+      }
       //successful
       res.json(all_articles);
-    })
+    });
 };
 
 //One requested article
@@ -19,11 +21,13 @@ exports.article_detail = function (req, res, next) {
   Article.findById(req.params.id)
     .populate("comments")
     .populate("author")
-    .exec(function(err, article) {
-      if (err) { return next(err); }
+    .exec(function (err, article) {
+      if (err) {
+        return next(err);
+      }
       //successful
       res.json(article);
-    })
+    });
 };
 
 //Get article create from end points on GET
@@ -45,11 +49,8 @@ exports.article_create_post = [
     .trim()
     .isLength({ min: 1 })
     .escape(),
-  body("body", "Body must not be empty.")
-    .trim()
-    .isLength({ min: 1 })
-    .escape(),
-  body('comments.*').escape(),
+  body("body", "Body must not be empty.").trim().isLength({ min: 1 }).escape(),
+  body("comments.*").escape(),
   body("author", "Author must not be empty.")
     .trim()
     .isLength({ min: 1 })
@@ -89,7 +90,7 @@ exports.article_create_post = [
       });
     }
   },
-]
+];
 
 //Article delete form data on GET
 exports.article_delete_get = function (req, res, next) {
@@ -107,34 +108,41 @@ exports.article_delete_get = function (req, res, next) {
 //Handle article delete on POST
 exports.article_delete_post = function (req, res, next) {
   Article.findByIdAndRemove(req.body.articleid, function deleteArticle(err) {
-    if (err) { next(err); }
+    if (err) {
+      next(err);
+    }
     //success - send success message
     res.sendStatus(200);
-  })
+  });
 };
 
 //Article update form data on GET
 exports.article_update_get = function (req, res, next) {
-  async.parallel({
-    article: function(callback) {
-      Article.findById(req.params.id)
-        .populate("comments")
-        .populate("author")
-        .exec(callback);
+  async.parallel(
+    {
+      article: function (callback) {
+        Article.findById(req.params.id)
+          .populate("comments")
+          .populate("author")
+          .exec(callback);
+      },
+      author: function (callback) {
+        Author.find(callback);
+      },
     },
-    author: function(callback) {
-      Author.find(callback);
-    },
-  }, function(err, results) {
-    if (err) { next(err); }
-    if (results.article == null) {
-      const err = new Error('Article not found');
-      err.status = 404;
-      return next(err);
+    function (err, results) {
+      if (err) {
+        next(err);
+      }
+      if (results.article == null) {
+        const err = new Error("Article not found");
+        err.status = 404;
+        return next(err);
+      }
+      //successful
+      res.json(results);
     }
-    //successful
-    res.json(results);
-  })
+  );
 };
 
 //Handle article update on POST
@@ -160,39 +168,51 @@ exports.article_update_post = [
       title: req.body.title,
       body: req.body.body,
       author: req.body.author,
-      _id: req.params.id
-    })
+      _id: req.params.id,
+    });
 
     if (!errors.isEmpty()) {
       //there are errors. send same json data as articel_update_get
-      async.parallel({
-        article: function(callback) {
-          Article.findById(req.params.id)
-            .populate("comments")
-            .populate("author")
-            .exec(callback);
+      async.parallel(
+        {
+          article: function (callback) {
+            Article.findById(req.params.id)
+              .populate("comments")
+              .populate("author")
+              .exec(callback);
+          },
+          author: function (callback) {
+            Author.find(callback);
+          },
         },
-        author: function(callback) {
-          Author.find(callback);
-        },
-      }, function(err, results) {
-        if (err) { next(err); }
-        if (results.article == null) {
-          const err = new Error('Article not found');
-          err.status = 404;
-          return next(err);
+        function (err, results) {
+          if (err) {
+            next(err);
+          }
+          if (results.article == null) {
+            const err = new Error("Article not found");
+            err.status = 404;
+            return next(err);
+          }
+          //successful
+          res.json(results);
         }
-        //successful
-        res.json(results);
-      })
+      );
       return;
     } else {
       //data from form is valid. update the record
-      Article.findByIdAndUpdate(req.params.id, article, {}, function(err, thearticle) {
-        if (err) { return next(err); }
-        //successful
-        res.sendStatus(200);
-      })
+      Article.findByIdAndUpdate(
+        req.params.id,
+        article,
+        {},
+        function (err, thearticle) {
+          if (err) {
+            return next(err);
+          }
+          //successful
+          res.sendStatus(200);
+        }
+      );
     }
-  }
-]
+  },
+];
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,57 +1,163 @@
 const User = require("../models/user");
+const { body, validationResult } = require("express-validator");
 
 //List of all users
 exports.user_list = function (req, res, next) {
-  res.json({
-    message: "User list not implemented",
+  User.find({}, "username").exec(function (err, all_users) {
+    if (err) {
+      return next(err);
+    }
+    //successful
+    res.json(all_users);
   });
 };
 
 //Details for a specific user
 exports.user_detail = function (req, res, next) {
-  res.json({
-    message: "User detail not implemented",
+  User.findById(req.params.id, "username email").exec(function (err, user) {
+    if (err) {
+      return next(err);
+    }
+    //successful
+    res.json(user);
   });
 };
 
 //User create on GET
 exports.user_create_get = function (req, res, next) {
   res.json({
-    message: "User create get not implemented",
+    message: "Create new user",
   });
 };
 
 //Handle user create on POST
-exports.user_create_post = function (req, res, next) {
-  res.json({
-    message: "User create post not implemented",
-  });
-};
+exports.user_create_post = [
+  //validate and sanitize fields
+  body("username", "Username must not be empty.")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("email", "Email must not be empty.")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("password", "Password must not be empty.")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+
+  //proccess request after validation and sanitization
+  (req, res, next) => {
+    //extract validation errors from a request
+    const errors = validationResult(req);
+
+    //create User object with escaped and trimmed data
+    let user = new User({
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password,
+    });
+
+    if (!errors.isEmpty()) {
+      //there are errors. send same json data as user_create_get
+      res.json({
+        message: "Create new user",
+      });
+      return;
+    } else {
+      //data is valid. save article
+      user.save(function (err) {
+        if (err) {
+          return next(err);
+        }
+        //successful
+        res.sendStatus(200);
+      });
+    }
+  },
+];
 
 //User delete on GET
 exports.user_delete_get = function (req, res, next) {
-  res.json({
-    message: "User delete get not implemented",
+  User.findById(req.params.id).exec(function (err, user) {
+    if (err) {
+      return next(err);
+    }
+    //successful
+    res.json(user);
   });
 };
 
 //User delete on POST
 exports.user_delete_post = function (req, res, next) {
-  res.json({
-    message: "User delete post not implemented",
+  User.findByIdAndRemove(req.body.userid, function deleteUser(err) {
+    if (err) {
+      next(err);
+    }
+    //success - send success message
+    res.sendStatus(200);
   });
 };
 
-//User update on GET
+//User update on GET FIXME
 exports.user_update_get = function (req, res, next) {
-  res.json({
-    message: "User update get not implemented",
+  User.findById(req.params.id).exec(function (err, user) {
+    if (err) {
+      next(err);
+    }
+    //successful
+    res.json(user);
   });
 };
 
 //User update on POST
-exports.user_update_post = function (req, res, next) {
-  res.json({
-    message: "User update post not implemented",
-  });
-};
+exports.user_update_post = [
+  //validate and sanitize fields
+  body("username", "Username must not be empty.")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("email", "Email must not be empty.")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("password", "Password must not be empty.")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+
+  //proccess request after validation and sanitization
+  (req, res, next) => {
+    //extreact the validation errors from a request
+    const errors = validationResult(req);
+
+    //create an user object with escaped/trimmed data and old id
+    let user = new User({
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password,
+      _id: req.params.id,
+    });
+
+    if (!errors.isEmpty()) {
+      //there are errors. send same json data as user_update_get
+      User.findById(req.params.id).exec(function (err, user) {
+        if (err) {
+          next(err);
+        }
+        //successful
+        res.json(user);
+      });
+      return;
+    } else {
+      //data from form is valid. update the record
+      User.findByIdAndUpdate(req.params.id, user, {}, function (err, theuser) {
+        if (err) {
+          return next(err);
+        }
+        //successful
+        res.sendStatus(200);
+      });
+    }
+  },
+];
